65cf072383f1677f3df2376aa338c774171880ce
brunodalcum <brunodalcum@gmail.com>
2020-12-28 20:32:29 -0300

    crud Categoria1

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
+use App\Model\Admin\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -10,5 +11,11 @@ class CategoryController extends Controller
     public function __construct()
     {
         $this->middleware('auth:admin');
+    }
+
+    public function category()
+    {
+        $category = Category::all();
+        return view('admin.category.category',compact('category'));
     }
 }
--- resources/views/admin/admin_layouts.blade.php
+++ resources/views/admin/admin_layouts.blade.php
@@ -37,6 +37,11 @@
     <link href="{{ asset('public/backend/lib/rickshaw/rickshaw.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css">
 
+    <link href="{{ asset('public/backend/lib/highlightjs/github.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/backend/lib/datatables/jquery.dataTables.css') }}" rel="stylesheet">
+    <link href="{{ asset('public/backend/lib/select2/css/select2.min.css') }}" rel="stylesheet">
+
+
     <!-- Starlight CSS -->
     <link rel="stylesheet" href="{{ asset('public/backend/css/starlight.css') }}">
 </head>
@@ -66,7 +71,7 @@
                 </div><!-- menu-item -->
             </a><!-- sl-menu-link -->
             <ul class="sl-menu-sub nav flex-column">
-                <li class="nav-item"><a href="chart-morris.html" class="nav-link">Categoria</a></li>
+                <li class="nav-item"><a href="{{ route('categories') }}" class="nav-link">Categoria</a></li>
                 <li class="nav-item"><a href="chart-flot.html" class="nav-link">Sub Categoria</a></li>
                 <li class="nav-item"><a href="chart-chartjs.html" class="nav-link">Marca</a></li>
             </ul>
@@ -352,6 +357,38 @@
 <script src="{{ asset('public/backend/lib/bootstrap/bootstrap.js') }}"></script>
 <script src="{{ asset('public/backend/lib/jquery-ui/jquery-ui.js') }}"></script>
 <script src="{{ asset('public/backend/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js') }}"></script>
+
+<script src="{{ asset('public/backend/lib/highlightjs/highlight.pack.js') }}"></script>
+<script src="{{ asset('public/backend/lib/datatables/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('public/backend/lib/datatables-responsive/dataTables.responsive.js') }}"></script>
+<script src="{{ asset('public/backend/lib/select2/js/select2.min.js') }}"></script>
+
+<script>
+    $(function(){
+        'use strict';
+
+        $('#datatable1').DataTable({
+            responsive: true,
+            language: {
+                searchPlaceholder: 'Search...',
+                sSearch: '',
+                lengthMenu: '_MENU_ items/page',
+            }
+        });
+
+        $('#datatable2').DataTable({
+            bLengthChange: false,
+            searching: false,
+            responsive: true
+        });
+
+        // Select2
+        $('.dataTables_length select').select2({ minimumResultsForSearch: Infinity });
+
+    });
+</script>
+
+
 <script src="{{ asset('public/backend/lib/jquery.sparkline.bower/jquery.sparkline.min.js') }}"></script>
 <script src="{{ asset('public/backend/lib/d3/d3.js') }}"></script>
 <script src="{{ asset('public/backend/lib/rickshaw/rickshaw.min.js') }}"></script>
