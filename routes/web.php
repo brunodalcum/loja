63e994eb5d7cba7144c8c213b84c0be90730be63
brunodalcum <brunodalcum@gmail.com>
2020-12-28 18:49:38 -0300

    layout ok

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {return view('layouts.app');});
+Route::get('/', function () {return view('pages.index');});
 //auth & user
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
